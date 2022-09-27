0aaee602f1cd17300ed3af265f44ee34f213b1b5
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-27 09:58:16 +0000

    completing the crud for projectd_view

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from projects.views import (list_projects, project_detail)
+from projects.views import (list_projects, project_detail, create_project, edit_project, delete_project)
 
 urlpatterns = [
     path("list-projects/",list_projects,name="list_projects"),
-    path("project-detail/<str:id>/", project_detail,name="project_detail")
+    path("project-detail/<str:id>/", project_detail,name="project_detail"),
+    path('create-project/', create_project,name="create_project"),
+    path('edit-project/<str:id>/', edit_project, name="edit_project"),
+    path('delete-project/<str:id>/', delete_project, name="delete_project")
 ]
